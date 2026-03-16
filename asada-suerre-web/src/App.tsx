ff4d7ac37aedb59a4e6793d8987248b9e5e74c87
Carlos Hernandez <krlosha30@gmail.com>
2026-03-16 10:48:47 -0600

    feat: changed from next.js to react boilerplate

---
+++ asada-suerre-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Blog from './blog/Blog'
+
+function App() {
+  return (
+    <Blog />
+  );
+}
+
+export default App;
